a015e2819ae8f118076f465ee6f38097608f29cf
Anton Shulga <shandm85@gmail.com>
2018-12-08 19:38:00 +0200

    Add Edit-action for Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\User;
 use App\Form\ArticleType;
 use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -57,7 +58,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/new", name="article_new")
      */
-    public function articleNew(Request $request)
+    public function articleNew(Request $request): Response
     {
         $article = new Article();
         
@@ -67,10 +68,53 @@ class ArticleController extends AbstractController
         
         if ($form->isSubmitted() && $form->isValid()) {
             $article = $form->getData();
-            dump($article);
+            dump($article); die;
+            
+            return $this->redirectToRoute('blog_list');
         }
         
         return $this->render('article/article_new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    /**
+     * @Route("/article/{slug}/edit", name="article_edit", requirements={"slug"="\d+"})
+     */
+    public function articleEdit(Request $request, $slug = 1): Response
+    {        
+        $article_arr = [];
+        foreach ($this->articles as $key => $article_val) {
+            if ($article_val['slug'] == $slug) {
+                $article_arr = $this->articles[$key];
+            }
+        }
+        //Create a test object while temporarily the database does not exist
+        $article = new Article();
+        $article->setTitle($article_arr['title']);
+        $article->setSlug($article_arr['slug']);
+        $article->setBody($article_arr['body']);
+//        $article->setPublishedAt($article_arr['publishedAt']);
+        
+        $user = new User();
+        $user->setFullName('authorTest');
+        $user->setUsername('authorTest');
+        $user->setEmail('author@test.com');
+        $user->setPassword('authorTest');
+        
+        $article->setAuthor($user);
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            dump($article); die;
+            
+            return $this->redirectToRoute('blog_list');
+        }
+        return $this->render('article/article_edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
