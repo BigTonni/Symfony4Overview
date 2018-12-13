50128b8c852cd75e15a0cd22464071184d8a7bdd
Anton Shulga <shandm85@gmail.com>
2018-12-13 22:25:15 +0200

    Rename tables

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -7,6 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
+ * @ORM\Table(name="comments")
  */
 class Comment
 {
