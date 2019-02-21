5a910b8fecba818d1c383eb6f91fd39824c56d05
Anton Shulga <shandm85@gmail.com>
2019-02-21 14:33:56 +0200

    Fix comment form

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,8 +17,7 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', TextareaType::class)
-            ->add('publishedAt', DateTimeType::class)
+            ->add('content', TextareaType::class, ['attr' => ['class' => 'mceNoEditor']])
             ->add('save', SubmitType::class, ['label' => 'Create Comment', 'attr' => ['class' => 'btn btn-primary']]);
     }
 
