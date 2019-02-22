71c4ca34ed70e896b2566a862aa9d8e4a7034523
Anton Shulga <shandm85@gmail.com>
2019-02-22 20:55:46 +0200

    Edit UserControllerTest and API ArticleControllerTest

--- tests/Controller/Api/ArticleControllerTest.php
+++ tests/Controller/Api/ArticleControllerTest.php
@@ -6,32 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ArticleControllerTest extends WebTestCase
 {
-//    public function testListArticles()
-//    {
-//        $client = static::createClient();
-//
-//        $client->request('GET', '/api/articles');
-//
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//    }
-//
-//    /**
-//     * @dataProvider provideUrls
-//     */
-//    public function testPageIsSuccessful($url)
-//    {
-//        $client = static::createClient();
-//        $client->request('GET', $url);
-//
-//        $this->assertTrue($client->getResponse()->isSuccessful());
-//    }
-//
-//    public function provideUrls()
-//    {
-//        return [
-//            ['/'],
-//            ['/api/doc'],
-//            ['/login']
-//        ];
-//    }
+    public function testListArticles()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/articles');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @dataProvider provideUrls
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function provideUrls()
+    {
+        return [
+            ['/'],
+            ['/api/doc'],
+            ['/login']
+        ];
+    }
 }
--- tests/Controller/Web/UserControllerTest.php
+++ tests/Controller/Web/UserControllerTest.php
@@ -24,6 +24,50 @@ class UserControllerTest extends WebTestCase
         );
     }
 
+    public function testEditUser()
+    {
+        $newUserEmail = 'test@author_new.com';
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'test@author1.com',
+            'PHP_AUTH_PW' => 'test',
+        ]);
+        $crawler = $client->request('GET', '/en/profile/edit');
+        $form = $crawler->selectButton('Save')->form([
+            'user[email]' => $newUserEmail,
+        ]);
+        $client->submit($form);
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+
+        $user = $client->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy([
+            'email' => $newUserEmail,
+        ]);
+        $this->assertNotNull($user);
+        $this->assertSame($newUserEmail, $user->getEmail());
+    }
+
+    public function testChangePassword()
+    {
+        $newUserPassword = 'new-password';
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'test@author_new.com',
+            'PHP_AUTH_PW' => 'test',
+        ]);
+        $crawler = $client->request('GET', '/en/profile/change-password');
+        $form = $crawler->selectButton('Save')->form([
+            'change_password[currentPassword]' => 'test',
+            'change_password[newPassword][first]' => $newUserPassword,
+            'change_password[newPassword][second]' => $newUserPassword,
+        ]);
+        $client->submit($form);
+        $response = $client->getResponse();
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertSame(
+            '/en/logout',
+            $response->getTargetUrl(),
+            'Changing password logout the user.'
+        );
+    }
+
     public function getUrlsForAnonymousUsers()
     {
         yield ['GET', '/en/profile/edit'];
