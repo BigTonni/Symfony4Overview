b18c5950ecda4a295dc439c110dd8bb962d135f3
Anton Shulga <shandm85@gmail.com>
2019-03-01 17:06:42 +0200

    Add API method to get token

---
+++ src/Controller/Api/TokenController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+
+/**
+ * @SWG\Tag(name="Tokens")
+*/
+class TokenController extends BaseRestController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+    /**
+     * Example test user - test@author1.com
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns user token"
+     * )
+     * @SWG\Parameter(
+     *     name="email",
+     *     in="query",
+     *     type="string",
+     *     required=true,
+     *     description="User email"
+     * )
+     * @Route("/tokens/current-user", methods={"GET"})
+     * @param Request $request
+     * @param JWTTokenManagerInterface $JWTManager
+     * @return JsonResponse
+     */
+    public function getUserTokenAction(Request $request, JWTTokenManagerInterface $JWTManager)
+    {
+        $email = $request->query->get('email') ?? false;
+
+        $user = $this->em
+            ->getRepository(User::class)
+            ->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->view($JWTManager->create($user), Response::HTTP_OK, [], [
+            'full',
+        ]);
+    }
+}
